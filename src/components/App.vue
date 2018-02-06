5cb542d4eff8f57cf469d76403ae86c5a1b38443
Pearse Hutson <pearse.2008@gmail.com>
2018-02-06 15:07:35 -0600

    Added ScorePage.vue

--- src/components/App.vue
+++ src/components/App.vue
@@ -8,8 +8,10 @@
                 </div>
                 <button>Back</button>
             </div>
-            <div class="dev-border flex-grow">Future Left Score Page</div>
-            <div class="dev-border flex-grow">Future Right Score Page</div>
+            <ScorePage />
+            <ScorePage />
+            <!-- <div class="dev-border flex-grow">Future Left Score Page</div>
+            <div class="dev-border flex-grow">Future Right Score Page</div> -->
             <div class="dev-border flex-col">
                 <div class="dev-border flex-grow">
                     Tools?
@@ -21,9 +23,14 @@
 </template>
 
 <script lang="ts">
+    import ScorePage from "./ScorePage.vue";
+    
     export default {
         data: function() {
             return {}
+        },
+        components: {
+            ScorePage
         }
     }
 </script>
---
+++ src/components/ScorePage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <img src="C:\Users\Pearse.Hutson\Pictures\bye bye\Bye-Bye-Birdie-Full-Score-Vol-1_Page_001.jpg" alt="">
+    </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style>
+
+</style>
