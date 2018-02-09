03cb57a7913ad8347cbd9f03f5f4cab3afbdcd63
Pearse Hutson <Pearse.2008@gmail.com>
2018-02-08 20:19:12 -0600

    Formatting

--- src/components/App.vue
+++ src/components/App.vue
@@ -23,57 +23,57 @@
 </template>
 
 <script>
-    import * as ScorePage from "./ScorePage.vue";
-    
-    export default {
-        data: function() {
-            return {
-                spHeight: 0,
-                spWidth: 0
-            }
-        },
-        components: {
-            ScorePage
-        },
-        methods: {
-            setSize() {
-                this.spHeight = this.$refs.scoreZone.clientHeight;
-                this.spWidth = this.$refs.scoreZone.clientWidth / 2;
-            }
-        },
-        mounted: function() {
-            this.setSize();
-            window.addEventListener("resize", this.setSize);
-        }
+import * as ScorePage from "./ScorePage.vue";
+
+export default {
+  data: function() {
+    return {
+      spHeight: 0,
+      spWidth: 0
+    };
+  },
+  components: {
+    ScorePage
+  },
+  methods: {
+    setSize() {
+      this.spHeight = this.$refs.scoreZone.clientHeight;
+      this.spWidth = this.$refs.scoreZone.clientWidth / 2;
     }
+  },
+  mounted: function() {
+    this.setSize();
+    window.addEventListener("resize", this.setSize);
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-    .height100 {
-        height: 100%;
-        max-height: 100%;
-    }
+.height100 {
+  height: 100%;
+  max-height: 100%;
+}
 
-    .width100 {
-        width: 100%;
-        max-width: 100%;
-    }
+.width100 {
+  width: 100%;
+  max-width: 100%;
+}
 
-    .flex-col {
-        display: flex;
-        flex-flow: column nowrap
-    }
+.flex-col {
+  display: flex;
+  flex-flow: column nowrap;
+}
 
-    .flex-row {
-        display: flex;
-        flex-flow: row nowrap;
-    }
+.flex-row {
+  display: flex;
+  flex-flow: row nowrap;
+}
 
-    .flex-grow {
-        flex-grow: 1;
-    }
+.flex-grow {
+  flex-grow: 1;
+}
 
-    .dev-border {
-        border: 1px solid black;
-    }
+.dev-border {
+  border: 1px solid black;
+}
 </style>
